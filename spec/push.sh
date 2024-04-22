46b9d6a971098efded102c34c8fc518d4b0ff5e6
Tornike Machitidze <Tornike_Machitidze@epam.com>
2024-04-22 13:37:42 +0400

    add script

---
+++ spec/push.sh
@@ -0,0 +1,8 @@
+if [ -z "$CI" ] && [ "$CI" != true ]; then
+    source "./.env";
+fi
+
+shopify auth logout;
+echo "----- PUSHING TO SHOPIFY";
+
+shopify theme push --store $DEV_THEME_STORE --theme $DEV_THEME_ID --password $DEV_THEME_PASSWORD --allow-live
